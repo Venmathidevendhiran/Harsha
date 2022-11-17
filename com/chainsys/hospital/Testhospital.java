a01ce10105ccf5b4df717f3d03889205baa74646
venm3342 <venm3342@CSLH3158.global.chainsys.com>
2022-11-17 12:46:52 +0530

    through eclipse

---
+++ com/chainsys/hospital/Testhospital.java
@@ -0,0 +1,21 @@
+package com.chainsys.hospital;
+
+
+public class Testhospital {
+
+	public static void main(String[] args) {
+		hospital hospital =new hospital(); 
+		hospital.hospitalName;
+		hospital.hospitalname="Appollo";
+		hospital.departments="Heart Specialist,Childcare" ;
+		hospital.address="Ayanambakkam";
+		hospital.ownername="Aravindh";
+		hospital.phoneNumber=9876543087l;
+		System.out.println("hospital.Name:"+hospital.hospitalname);
+		System.out.println("hospital.Departments:"+hospital.departments);
+		System.out.println("hospital.Address:"+hospital.address);
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ com/chainsys/hospital/hospital.java
@@ -0,0 +1,16 @@
+package com.chainsys.hospital;
+
+public class hospital 
+ {
+
+	
+		String hospitalName;
+		String departments;
+		String address ;
+		String ownerName;
+		long phoneNumber;
+		
+
+	}
+
+
