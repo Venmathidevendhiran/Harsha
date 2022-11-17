1d6b307c96fc0c256ccc76f929495dc3a8833380
Venmathi D <venm3342@global.chainsys.com>
2022-11-17 12:41:42 +0530

    through cmd

---
+++ com/novbatch/Employee.java
@@ -0,0 +1,21 @@
+package com.novbatch;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		int empId=5678;
+		char gender='m';
+		String name ="Aravind";
+		String domain ="Trainee";
+		long phoneNumber = 9787486803l;
+		boolean graduate =true;
+		String bloodGroup=" A+ve ",address ="Ayanambakkam,chennai 90",mailId="venmathi.chainsys.com";
+		System.out.println("Employee name:"+name +"\n" +"EmployeeId :" + empId  +"");		
+		System.out.println(graduate);
+
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
