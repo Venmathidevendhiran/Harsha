5432d2380702652035518bf34569a62536fdfa8b
venm3342 <venm3342@CSLH3158.global.chainsys.com>
2022-11-17 12:55:45 +0530

    through eclipse

---
+++ com/chainsys/hospital/hospital2.java
@@ -0,0 +1,16 @@
+package com.chainsys.hospital;
+
+public class hospital2 
+ {
+
+	
+		String hospitalName;
+		String departments;
+		String address ;
+		String ownerName;
+		long phoneNumber;
+		
+
+	}
+
+
